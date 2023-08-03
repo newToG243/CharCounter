6d77bfb0c08ec68521c7a48f545dc4995978225a
newToG243 <122686416+newToG243@users.noreply.github.com>
2023-08-04 00:12:04 +0530

    Add files via upload

---
+++ charcounterapp/index.js
@@ -0,0 +1,24 @@
+const charval=document.getElementById("textarea");
+let totalcount=document.getElementById("total-counter");
+let remainingcount=document.getElementById("remaining-counter");
+
+let userchar=0;
+const updatecounter=()=>{
+userchar=charval.value.length;
+
+totalcount.innerText=userchar;
+remainingcount.innerText=150-userchar;  
+}
+charval.addEventListener("keyup",()=>updatecounter());
+// copytext
+const copyText=()=>{
+    charval.select();
+    charval.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(charval.value);
+}
+
+
+
+
+
+
